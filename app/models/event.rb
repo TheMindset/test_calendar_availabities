ccb14c27f9892492a19416d1108d710812c7cc84
TheMindset <mulezig2014@outlook.fr>
2019-10-04 15:13:45 +0200

    available slots

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,39 @@
 class Event < ActiveRecord::Base
   
   def self.availabilities(date)
-    calendar = [
-      {date: date, slots: []}
-    ]
-    6.times { calendar << {date: date += 1, slots: []} }
+    calendar = []
+    calendar << { date: date, slots: [] }
+    6.times do
+      calendar << { date: date += 1, slots: check_slots(date) }
+    end
     calendar
   end
+
+    def self.check_slots(date)
+      open_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'opening'", date, true)
+      close_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'appointment'", date, true)
+
+      open_list = []
+      close_list = []
+      time_slot = 30.minutes
+      free_slot = []
+      open_slot.each do |slot|
+        while slot.starts_at < slot.ends_at do
+          open_list << slot.starts_at.strftime('%k:%M').strip
+          slot.starts_at += time_slot
+        end
+      end
+
+      close_slot.each do |slot|
+        while slot.starts_at < slot.ends_at do
+          close_list << slot.starts_at.strftime('%k:%M').strip
+          slot.starts_at += time_slot
+        end
+      end
+
+      free_slots = open_list - close_list
+      free_slots
+    end
+
+
 end
