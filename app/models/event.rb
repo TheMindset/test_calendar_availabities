13e2d1011b6b82e97d8cf64669b0b340775384aa
TheMindset <mulezig2014@outlook.fr>
2019-10-04 16:15:51 +0200

    add some test & clean code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
   
+  validates :starts_at, :ends_at, :kind, presence: true
+  validates :kind, inclusion: { in: ['opening', 'appointment'] }
+
   def self.availabilities(date)
     calendar = []
     calendar << { date: date, slots: [] }
@@ -9,31 +12,30 @@ class Event < ActiveRecord::Base
     calendar
   end
 
-    def self.check_slots(date)
-      open_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'opening'", date, true)
-      close_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'appointment'", date, true)
+  def self.check_slots(date)
+    open_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'opening'", date, true)
+    close_slot = Event.where("(starts_at >= ? OR weekly_recurring = ?) AND kind = 'appointment'", date, true)
 
-      open_list = []
-      close_list = []
-      time_slot = 30.minutes
-      free_slot = []
-      open_slot.each do |slot|
-        while slot.starts_at < slot.ends_at do
-          open_list << slot.starts_at.strftime('%k:%M').strip
-          slot.starts_at += time_slot
-        end
+    open_list = []
+    close_list = []
+    time_slot = 30.minutes
+    
+    open_slot.each do |slot|
+      while slot.starts_at < slot.ends_at do
+        open_list << slot.starts_at.strftime('%k:%M').strip
+        slot.starts_at += time_slot
       end
-
-      close_slot.each do |slot|
-        while slot.starts_at < slot.ends_at do
-          close_list << slot.starts_at.strftime('%k:%M').strip
-          slot.starts_at += time_slot
-        end
-      end
-
-      free_slots = open_list - close_list
-      free_slots
     end
 
+    close_slot.each do |slot|
+      while slot.starts_at < slot.ends_at do
+        close_list << slot.starts_at.strftime('%k:%M').strip
+        slot.starts_at += time_slot
+      end
+    end
+
+    free_slots = open_list - close_list
+    free_slots
+  end
 
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
 
+  def setup
+    @event = Event.create kind: 'Hercule', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+    @invalid_event = Event.create kind: 'opening' , starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: "" , weekly_recurring: true
+  end
   test "one simple test example" do
     Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
     Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
@@ -12,8 +16,16 @@ class EventTest < ActiveSupport::TestCase
     assert_equal [], availabilities[0][:slots]
     assert_equal Date.new(2014, 8, 11), availabilities[1][:date]
     assert_equal ["9:30", "10:00", "11:30", "12:00"], availabilities[1][:slots]
-    # assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
-    # assert_equal 7, availabilities.length
+    assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
+    assert_equal 7, availabilities.length
   end
 
+
+  test "should validates kind of events" do
+    refute @event.valid?
+  end
+
+  test "should validates input presence" do
+    assert @invalid_event.errors.messages.include?(:ends_at)
+  end
 end
